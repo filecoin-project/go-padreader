69421a0f17b6910fe7945a9c7c582a57de9a0c69
laser <l@s3r.me>
2020-02-10 12:24:57 -0800

    return UnpaddedPieceSize

--- padreader.go
+++ padreader.go
@@ -9,16 +9,16 @@ import (
 
 // PaddedSize takes size to the next power of two and then returns the number of
 // not-bit-padded bytes that would fit into a sector of that size.
-func PaddedSize(size uint64) uint64 {
+func PaddedSize(size uint64) abi.UnpaddedPieceSize {
 	logv := 64 - bits.LeadingZeros64(size)
 
 	sectSize := uint64(1 << logv)
-	bound := uint64(abi.PaddedPieceSize(sectSize).Unpadded())
-	if size <= bound {
+	bound := abi.PaddedPieceSize(sectSize).Unpadded()
+	if size <= uint64(bound) {
 		return bound
 	}
 
-	return uint64(abi.PaddedPieceSize(1 << (logv + 1)).Unpadded())
+	return abi.PaddedPieceSize(1 << (logv + 1)).Unpadded()
 }
 
 type nullReader struct{}
@@ -33,11 +33,12 @@ func (nr nullReader) Read(b []byte) (int, error) {
 
 // New produces a reader that produces the provided reader's bytes with a suffix
 // of NUL bytes.
-func New(r io.Reader, size uint64) (io.Reader, uint64) {
+func New(r io.Reader, size uint64) (io.Reader, abi.UnpaddedPieceSize) {
 	padSize := PaddedSize(size)
+	n := uint64(padSize)
 
 	return io.MultiReader(
 		io.LimitReader(r, int64(size)),
-		io.LimitReader(nullReader{}, int64(padSize-size)),
+		io.LimitReader(nullReader{}, int64(n-size)),
 	), padSize
 }
--- padreader_test.go
+++ padreader_test.go
@@ -2,18 +2,20 @@ package padreader
 
 import (
 	"testing"
-	
+
+	"github.com/filecoin-project/specs-actors/actors/abi"
+
 	"github.com/stretchr/testify/assert"
 )
 
 func TestComputePaddedSize(t *testing.T) {
-	assert.Equal(t, uint64(1040384), PaddedSize(1000000))
+	assert.Equal(t, abi.UnpaddedPieceSize(1040384), PaddedSize(1000000))
 
-	assert.Equal(t, uint64(1016), PaddedSize(548))
-	assert.Equal(t, uint64(1016), PaddedSize(1015))
-	assert.Equal(t, uint64(1016), PaddedSize(1016))
-	assert.Equal(t, uint64(2032), PaddedSize(1017))
+	assert.Equal(t, abi.UnpaddedPieceSize(1016), PaddedSize(548))
+	assert.Equal(t, abi.UnpaddedPieceSize(1016), PaddedSize(1015))
+	assert.Equal(t, abi.UnpaddedPieceSize(1016), PaddedSize(1016))
+	assert.Equal(t, abi.UnpaddedPieceSize(2032), PaddedSize(1017))
 
-	assert.Equal(t, uint64(2032), PaddedSize(1024))
-	assert.Equal(t, uint64(4064), PaddedSize(2048))
+	assert.Equal(t, abi.UnpaddedPieceSize(2032), PaddedSize(1024))
+	assert.Equal(t, abi.UnpaddedPieceSize(4064), PaddedSize(2048))
 }
